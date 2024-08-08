42ce9113b579b5a4eaedf1b20558ee1cdcf64558
Dariet-SP <159501959+Dariet-SP@users.noreply.github.com>
2024-08-08 21:37:23 +0000

    second commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 /* eslint-disable */
-//
+// First, we need to create the function that creates cards
+// Second, another one that creates random numbers and symbols
+// Third, we need a function that injects the number of cards requested by the user
+// Fourth, must
 import "bootstrap";
 import "./style.css";
 
 window.onload = function() {
   //write your code here
+  // First, we need to create the function that creates cards
   let suits = ["♦", "♥", "♠", "♣"];
   let number = [
     "A",
@@ -21,28 +25,50 @@ window.onload = function() {
     "Q",
     "K"
   ];
-  let redOnes = ["♦", "♥"];
+  // let redOnes = ["♦", "♥"];
+  // Second, another one that creates random numbers and symbols
 
-  function createSN() {
-    const randomSuits = Math.floor(Math.random() * suits.length);
-    const randomNumber = Math.floor(Math.random() * number.length);
-    const suit = suits[randomSuits];
-    const symbolElement = document.getElementById("SIsymbol");
-    const symbolElement2 = document.getElementById("SIsymbol2");
-
-    symbolElement.innerHTML = suit;
-    symbolElement2.innerHTML = suit;
-
-    if (redOnes.includes(suit)) {
-      symbolElement.style.color = "red";
-      symbolElement2.style.color = "red";
-    } else {
-      symbolElement.style.color = "black";
-      symbolElement2.style.color = "black";
-    }
-
-    document.getElementById("numb3r").innerHTML = number[randomNumber];
+  function completeCard() {
+    const suit = Math.floor(Math.random() * suits.length);
+    const numb3r = Math.floor(Math.random() * number.length);
+    return { suit, numb3r };
+  }
+  function createCards() {
+    const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = "";
+    cardList.forEach(card => {
+      const cardHTML = `
+        <div class="card" style="width: 100px; height: 150px;">
+          <div class="card-body row row-cols-1">
+            <div style="float: left;">
+              <p>${card.suit}</p>
+            </div>
+            <div class="align-self-center" style="text-align: center; ">
+              <p>${card.number}</p>
+            </div>
+            <div style="float: right; transform: rotate(180deg);">
+              <p>${card.suit}</p>
+            </div>
+          </div>
+        </div>
+      `;
+      cardContainer.innerHTML += cardHTML;
+    });
   }
 
-  createSN();
+  function createCards(numCards) {
+    cardList = [];
+    for (let i = 0; i < numCards; i++) {
+      cardList.push(completeCard());
+    }
+    renderCards();
+  }
+
+  const numCardsInput = document.getElementById("myInput");
+  const generateButton = document.getElementById("myDraw");
+
+  generateButton.addEventListener("click", () => {
+    const numCards = parseInt(numCardsInput.value);
+    createCards(numCards);
+  });
 };
