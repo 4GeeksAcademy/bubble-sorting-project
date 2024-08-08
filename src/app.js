384139eb47eab78a8954a429e737a2d0cd13dc0f
Dariet-SP <159501959+Dariet-SP@users.noreply.github.com>
2024-08-08 20:16:49 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,48 @@
 /* eslint-disable */
+//
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = ["♦", "♥", "♠", "♣"];
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let redOnes = ["♦", "♥"];
+
+  function createSN() {
+    const randomSuits = Math.floor(Math.random() * suits.length);
+    const randomNumber = Math.floor(Math.random() * number.length);
+    const suit = suits[randomSuits];
+    const symbolElement = document.getElementById("SIsymbol");
+    const symbolElement2 = document.getElementById("SIsymbol2");
+
+    symbolElement.innerHTML = suit;
+    symbolElement2.innerHTML = suit;
+
+    if (redOnes.includes(suit)) {
+      symbolElement.style.color = "red";
+      symbolElement2.style.color = "red";
+    } else {
+      symbolElement.style.color = "black";
+      symbolElement2.style.color = "black";
+    }
+
+    document.getElementById("numb3r").innerHTML = number[randomNumber];
+  }
+
+  createSN();
 };
